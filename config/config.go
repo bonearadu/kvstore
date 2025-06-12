85fb753eebc034e6b6dc75269ec5622114d2bf32
Radu Alexandru Bonea <radbonea@amazon.com>
2025-06-12 09:23:41 +0100

    WIP

--- config/config.go
+++ config/config.go
@@ -6,6 +6,7 @@ type StoreImpl int
 
 const (
 	InMemory StoreImpl = iota
+	InMemorySnap
 	Persistent
 	PersistentCached
 )
@@ -25,9 +26,9 @@ func ParseFlags() *ServerConfig {
 
 	flag.IntVar(&config.Port, "port", 8080, "Port to listen on")
 	flag.IntVar(&mode, "mode", 0,
-		"The key-value store implementation to use. 0 = In-Memory map, 1 = Persistent KV store, "+
-			"2 = Persistent KV store with caching")
-	flag.StringVar(&config.StorePath, "store_path", "", "The path for the persistent storage, if used")
+		"The key-value store implementation to use. 0 = In-Memory map, 1 = In-Memory with snapshotting"+
+			"2 = Persistent KV store, 3 = Persistent KV store with caching")
+	flag.StringVar(&config.StorePath, "store_path", "", "The path for the persistent storage or snapshots, if used")
 	flag.IntVar(&config.CacheCapacity, "cache_capacity", 100,
 		"The size of the cache for the persistent cached storage, if used")
 	flag.Parse()
